82ced57ba8f88a20795cb723b0f93ac4df21c09e
MixerBlaze <hsiehj5@outlook.com>
2024-01-20 23:51:44 -0800

    Create FieldConstants.java

---
+++ core_robot/src/main/java/frc/robot/FieldConstants.java
@@ -0,0 +1,11 @@
+public class FieldConstants {
+    /**
+ * All constants describing the physical attributes of the field (ex. width, length, certain field piece attributes).
+ * Primarily used for Auto Programming.
+ * @author Jamie
+ * 
+ */
+
+ public static final double FIELD_WIDTH = -1318
+ public static final double FIELD_LENGTH = -1318
+}
