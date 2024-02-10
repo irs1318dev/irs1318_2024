cc4f92e552c8bf1c845f74229b9846fb00043655
William Rall <wm_rall@hotmail.com>
2024-02-10 15:31:41 -0800

    Add some logic for pose estimation for fixing skew when turning during

--- core_robot/src/main/java/frc/lib/helpers/Pair.java
+++ core_robot/src/main/java/frc/lib/helpers/Pair.java
@@ -5,12 +5,38 @@ package frc.lib.helpers;
  */
 public class Pair<T1, T2>
 {
-    public final T1 first;
-    public final T2 second;
+    private T1 first;
+    private T2 second;
 
     public Pair(T1 first, T2 second)
     {
         this.first = first;
+        this.second = second;
+    }
+
+    public T1 getFirst()
+    {
+        return this.first;
+    }
+
+    public T2 getSecond()
+    {
+        return this.second;
+    }
+
+    public void set(T1 first, T2 second)
+    {
+        this.first = first;
+        this.second = second;
+    }
+
+    public void setFirst(T1 first)
+    {
+        this.first = first;
+    }
+
+    public void setSecond(T2 second)
+    {
         this.second = second;
     }
 }
