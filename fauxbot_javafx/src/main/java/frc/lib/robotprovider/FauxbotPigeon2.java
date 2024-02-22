dfeb2ca9f49cd44059cceb818b46d513e30ee87e
William Rall <wm_rall@hotmail.com>
2024-02-22 01:05:10 -0800

    Add libGDX-style robot simulator

---
+++ fauxbot_javafx/src/main/java/frc/lib/robotprovider/FauxbotPigeon2.java
@@ -0,0 +1,37 @@
+package frc.lib.robotprovider;
+
+public class FauxbotPigeon2 extends FauxbotIMU implements IPigeon2
+{
+    public FauxbotPigeon2(int deviceNumber)
+    {
+        super();
+
+        FauxbotSensorManager.set(new FauxbotSensorConnection(FauxbotSensorConnection.SensorConnector.CAN, this.getClass(), deviceNumber), this);
+    }
+
+    public void getYawPitchRoll(double[] ypr_deg)
+    {
+        ypr_deg[0] = this.get();
+    }
+
+    public void getRawGyro(double[] xyz_dps)
+    {
+    }
+
+    public void setYaw(double angleDeg)
+    {
+        this.set(angleDeg);
+    }
+
+    public void getRollPitchYawRates(double[] xyz_dps)
+    {
+    }
+
+    public void setYPRUpdateFrequency(double frequencyHz)
+    {
+    }
+
+    public void setRPYRateUpdateFrequency(double frequencyHz)
+    {
+    }
+}
---
+++ fauxbot_javafx/src/main/java/frc/lib/robotprovider/FauxbotTalonFX.java
@@ -0,0 +1,217 @@
+package frc.lib.robotprovider;
+
+import frc.robot.IRealWorldSimulator;
+import frc.lib.controllers.PIDHandler;
+
+public class FauxbotTalonFX extends FauxbotAdvancedMotorBase implements ITalonFX
+{
+    private final IRealWorldSimulator simulator;
+
+    private FauxbotEncoder innerEncoder;
+    private PIDHandler pidHandler;
+
+    private TalonFXControlMode currentMode;
+    private double kp;
+    private double ki;
+    private double kd;
+    private double kf;
+
+    public FauxbotTalonFX(int deviceNumber, IRealWorldSimulator simulator)
+    {
+        super(deviceNumber);
+
+        this.simulator = simulator;
+        this.currentMode = TalonFXControlMode.PercentOutput;
+
+        this.innerEncoder = new FauxbotEncoder(new FauxbotSensorConnection(FauxbotSensorConnection.SensorConnector.CAN, FauxbotTalonFX.class, this.connection.getPort()));
+    }
+
+    public void follow(ITalonFX talonFX)
+    {
+    }
+
+    public void follow(ITalonFX talonFX, boolean invertDirection)
+    {
+    }
+
+    public void setControlMode(TalonFXControlMode mode)
+    {
+        this.currentMode = mode;
+        this.resetPID();
+    }
+
+    @Override
+    public void clearRemoteSensor()
+    {
+    }
+
+    @Override
+    public void setRemoteSensor(int sensorId, double ratio)
+    {
+    }
+
+    @Override
+    public void setFeedbackUpdateRate(double frequencyHz)
+    {
+    }
+
+    @Override
+    public void setErrorUpdateRate(double frequencyHz)
+    {
+    }
+
+    @Override
+    public void updateLimitSwitchConfig(boolean forwardEnabled, boolean forwardNormallyOpen, boolean forwardReset, double forwardResetPosition, boolean reverseEnabled, boolean reverseNormallyOpen, boolean reverseReset, double reverseResetPosition)
+    {
+    }
+
+    @Override
+    public void setSupplyCurrentLimit(boolean enabled, double currentLimit, double triggerThresholdCurrent, double triggerThresholdTime)
+    {
+    }
+
+    public void setSelectedSlot(int slotId)
+    {
+    }
+
+    public void setPIDF(double p, double i, double d, double f, int slotId)
+    {
+        this.kp = p;
+        this.ki = i;
+        this.kd = d;
+        this.kf = f;
+        this.resetPID();
+    }
+
+    public void setMotionMagicPIDVS(double p, double i, double d, double v, double s, double cruiseVelocity, double maxAcceleration, double maxJerk, int slotId)
+    {
+    }
+
+    @Override
+    public void set(double value)
+    {
+        this.set(this.currentMode, 0, value, 0.0);
+    }
+
+    @Override
+    public void set(TalonFXControlMode mode, double value)
+    {
+        this.set(mode, 0, value, 0.0);
+    }
+
+    @Override
+    public void set(double value, double feedForward)
+    {
+        this.set(this.currentMode, 0, value, feedForward);
+    }
+
+    @Override
+    public void set(TalonFXControlMode mode, double value, double feedForward)
+    {
+        this.set(mode, 0, value, feedForward);
+    }
+
+    @Override
+    public void set(TalonFXControlMode mode, int slotId, double value)
+    {
+        this.set(mode, slotId, value, 0.0);
+    }
+
+    @Override
+    public void set(TalonFXControlMode mode, int slotId, double value, double feedForward)
+    {
+        if (mode == TalonFXControlMode.Follower)
+        {
+            FauxbotActuatorBase actuator = FauxbotActuatorManager.get(new FauxbotActuatorConnection(FauxbotActuatorConnection.ActuatorConnector.CAN, (int)value));
+            if (actuator != null && actuator instanceof FauxbotAdvancedMotorBase)
+            {
+                FauxbotAdvancedMotorBase advancedMotor = (FauxbotAdvancedMotorBase)actuator;
+                advancedMotor.currentPowerProperty.addListener(
+                    (observable, oldValue, newValue) -> { this.currentPowerProperty.set((Double)newValue); });
+            }
+            else
+            {
+                throw new RuntimeException("expected a different actuator type " + actuator == null ? "null" : actuator.toString());
+            }
+        }
+        else if (mode == TalonFXControlMode.Velocity && this.pidHandler != null)
+        {
+            super.set(this.pidHandler.calculateVelocity(value, innerEncoder.getRate()) + feedForward);
+        }
+        else if (mode == TalonFXControlMode.Position && this.pidHandler != null)
+        {
+            super.set(this.pidHandler.calculatePosition(value, innerEncoder.get()) + feedForward);
+        }
+        else
+        {
+            super.set(value + feedForward);
+        }
+    }
+
+    public void updateLimitSwitchConfig(boolean forwardEnabled, boolean forwardNormallyOpen, boolean reverseEnabled, boolean reverseNormallyOpen)
+    {
+    }
+
+    public void setMotorOutputSettings(boolean invert, MotorNeutralMode neutralMode)
+    {
+    }
+
+    public void setInvertSensor(boolean flip)
+    {
+    }
+
+    public void setNeutralMode(MotorNeutralMode neutralMode)
+    {
+    }
+
+    public void setVoltageCompensation(boolean enabled, double maxVoltage)
+    {
+    }
+
+    public void stop()
+    {
+    }
+
+    public void setPosition(double position)
+    {
+    }
+
+    public void reset()
+    {
+    }
+
+    public double getPosition()
+    {
+        return this.innerEncoder.getDistance();
+    }
+
+    public double getVelocity()
+    {
+        return 0.0;
+    }
+
+    public double getError()
+    {
+        return 0.0;
+    }
+
+    public TalonXLimitSwitchStatus getLimitSwitchStatus()
+    {
+        return new TalonXLimitSwitchStatus(false, false);
+    }
+
+    private void resetPID()
+    {
+        if (this.simulator.shouldSimulatePID() &&
+            (this.currentMode == TalonFXControlMode.Position || this.currentMode == TalonFXControlMode.Velocity))
+        {
+            ITimer timer = new FauxbotTimer();
+            timer.start();
+            this.pidHandler = new PIDHandler(this.kp, this.ki, this.kd, this.kf, 1.0, -4096.0, 4096.0, timer);
+        }
+        else
+        {
+            this.pidHandler = null;
+        }
+    }
+}
