220805774c452d083db079103e8d78f6ff63c7a2
irs1318ds <58891121+irs1318ds@users.noreply.github.com>
2024-03-07 20:46:52 -0800

    Commenting incomplete stuff

--- core_robot/src/main/java/frc/robot/driver/controltasks/ShootTrapTask.java
+++ core_robot/src/main/java/frc/robot/driver/controltasks/ShootTrapTask.java
@@ -1,64 +1,64 @@
-package frc.robot.driver.controltasks;
+// package frc.robot.driver.controltasks;
 
-import frc.robot.TuningConstants;
-import frc.robot.FieldConstants;
-import frc.robot.HardwareConstants;
+// import frc.robot.TuningConstants;
+// import frc.robot.FieldConstants;
+// import frc.robot.HardwareConstants;
 
-import frc.lib.driver.IControlTask;
-import frc.lib.helpers.Helpers;
-import frc.robot.driver.*;
-import frc.robot.mechanisms.*;
+// import frc.lib.driver.IControlTask;
+// import frc.lib.helpers.Helpers;
+// import frc.robot.driver.*;
+// import frc.robot.mechanisms.*;
 
 
-public class ShootTrapTask extends DecisionSequentialTask
-{
-    private boolean hasCompleted;
+// public class ShootTrapTask extends DecisionSequentialTask
+// {
+//     private boolean hasCompleted;
 
     
-    public ShootTrapTask ()
-    {
-    }
+//     public ShootTrapTask ()
+//     {
+//     }
 
 
-    @Override
-    public void begin()
-    {
-        hasCompleted = false;
-        this.AppendTask(new ApproachAprilTagTask(TRAP_APRILTAG_TO_ROBOT_X, TRAP_APRILTAG_TO_ROBOT_Y, ));
-    }
+//     @Override
+//     public void begin()
+//     {
+//         hasCompleted = false;
+//         this.AppendTask(new ApproachAprilTagTask(TRAP_APRILTAG_TO_ROBOT_X, TRAP_APRILTAG_TO_ROBOT_Y, ));
+//     }
 
-    @Override
-    protected void finishedTask(IControlTask finishedTask)
-    {
-        super.finishedTask(finishedTask);
+//     @Override
+//     protected void finishedTask(IControlTask finishedTask)
+//     {
+//         super.finishedTask(finishedTask);
 
-        if (finishedTask instanceof SequentialTask) {
-            hasCompleted = true;
-        }
-    }
+//         if (finishedTask instanceof SequentialTask) {
+//             hasCompleted = true;
+//         }
+//     }
 
-    /*
-     * Update the current task and controls
-     */
-    @Override
-    public void update()
-    {
+//     /*
+//      * Update the current task and controls
+//      */
+//     @Override
+//     public void update()
+//     {
         
-    }
+//     }
 
-    /**
-     * End the current task and reset control changes appropriately
-     */
-    @Override
-    public void end()
-    {
+//     /**
+//      * End the current task and reset control changes appropriately
+//      */
+//     @Override
+//     public void end()
+//     {
 
-    }
+//     }
 
-    @Override
-    public boolean hasCompleted()
-    {
-        return hasCompleted;
-    }
+//     @Override
+//     public boolean hasCompleted()
+//     {
+//         return hasCompleted;
+//     }
     
-}
+// }
