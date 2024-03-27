dd8deb3a7bbd90eb8584847c617a128bcbeb2be7
NisKuMo <nishant.mondal@gmail.com>
2024-03-26 20:19:39 -0700

    SpeakerAbsoluteOrientationTask made
    Co-authored-by: Arjun Pervaje <ArjunPervaje@users.noreply.github.com>

---
+++ core_robot/src/main/java/frc/robot/driver/controltasks/SpeakerAbsoluteOrientationTask.java
@@ -0,0 +1,83 @@
+package frc.robot.driver.controltasks;
+
+import frc.lib.driver.IDriver;
+import frc.lib.helpers.Helpers;
+import frc.lib.mechanisms.LoggingManager;
+import frc.lib.robotprovider.IRobotProvider;
+import frc.robot.AutonLocManager;
+import frc.robot.TuningConstants;
+import frc.robot.mechanisms.OffboardVisionManager;
+import frc.robot.mechanisms.PigeonManager;
+
+public class SpeakerAbsoluteOrientationTask extends PIDTurnTaskBase
+{
+    private double absoluteXSpeakerWResToCenter;
+    private double absoluteYSpeakerWResToCenter;
+    private double absoluteXRobotWResToCenter;
+    private double absoluteYRobotWResToCenter;
+    private double orientationTheta;
+    
+    private boolean isRedAlliance;
+    
+    private PigeonManager pigeonManager;
+    private IDriver driver;
+    private AutonLocManager manager;
+    private IRobotProvider provider;
+    private LoggingManager loggingManager;
+    private OffboardVisionManager visionManager;
+
+
+    /**
+     * 
+     */
+    public SpeakerAbsoluteOrientationTask(
+        boolean bestEffort,
+        boolean useTime,
+        AutonLocManager manager,
+        IRobotProvider provider,
+        OffboardVisionManager visionManager)
+    {
+        super(useTime,bestEffort);
+        //parameter initializations
+        //to determine position of the speaker depending on the side
+        this.absoluteXSpeakerWResToCenter = TuningConstants.CENTER_TO_SPEAKER_X_DISTANCE;
+        this.absoluteYSpeakerWResToCenter = TuningConstants.CENTER_TO_SPEAKER_Y_DISTANCE;
+    }
+
+
+    @Override
+    public void begin()
+    {
+        super.begin();
+
+        
+
+        this.driver = this.getInjector().getInstance(IDriver.class);
+        this.provider = this.getInjector().getInstance(IRobotProvider.class);
+        this.visionManager = this.getInjector().getInstance(OffboardVisionManager.class);
+        this.pigeonManager = this.getInjector().getInstance(PigeonManager.class);
+
+        this.manager = new AutonLocManager(this.getInjector().getInstance(IRobotProvider.class));
+        this.pigeonManager = new PigeonManager(this.driver,this.loggingManager,this.provider);
+        this.isRedAlliance = manager.getRedUpdateAlliance();
+
+    }
+
+
+    @Override
+    protected Double getHorizontalAngle()
+    {
+        // TODO Auto-generated method stub
+        this.absoluteXRobotWResToCenter = this.visionManager.getAbsX();
+        this.absoluteYRobotWResToCenter = this.visionManager.getAbsY();
+
+        double absoluteDeltaXSpeakerToRobot = this.absoluteXRobotWResToCenter - this.absoluteXSpeakerWResToCenter;
+        double absoluteDeltaYSpeakerToRobot = this.absoluteYRobotWResToCenter - this.absoluteYSpeakerWResToCenter;
+        
+        this.orientationTheta = this.pigeonManager.getYaw() - Helpers.atan2d(
+            absoluteDeltaXSpeakerToRobot,
+            absoluteDeltaYSpeakerToRobot);
+
+        return (this.isRedAlliance) ? this.orientationTheta : - 1 * this.orientationTheta;
+    }
+}
