a0460257a98d49800f8dc80d68bfaff1bfb92d3a
irs1318ds <58891121+irs1318ds@users.noreply.github.com>
2024-03-28 20:31:01 -0700

    Create AbsolutePosShotTask.java

---
+++ core_robot/src/main/java/frc/robot/driver/controltasks/AbsolutePosShotTask.java
@@ -0,0 +1,12 @@
+package frc.robot.driver.controltasks;
+
+import frc.robot.mechanisms.OffboardVisionManager;
+
+public class AbsolutePosShotTask {
+    double absX;
+
+    //use offboardvision manager to grab absolute x and y
+    //orient towards the speaker apriltag or any other desired apriltag
+    //shoot
+    //use append task??
+}
